0536103424a359083d57e3b3913fd4c9e3904fd6
Matteo Baglini <matteo.baglini@gmail.com>
2017-11-27 19:24:55 +0100

    Implement 'apply' and 'map2' combinators

--- scala/src/main/scala/org/fprefactoring/App.scala
+++ scala/src/main/scala/org/fprefactoring/App.scala
@@ -7,14 +7,26 @@ object App {
   trait BoolResult[+A] {
     def map[B](f: A => B): BoolResult[B]
     def flatMap[B](f: A => BoolResult[B]): BoolResult[B]
+    def apply[B](cfab: BoolResult[A => B]): BoolResult[B]
+    def map2[B, C](cb: BoolResult[B])(f: (A, B) => C): BoolResult[C]
   }
   case class TrueResult[A](value: A) extends BoolResult[A] {
     override def map[B](f: (A) => B): BoolResult[B] = TrueResult(f(value))
     override def flatMap[B](f: (A) => BoolResult[B]): BoolResult[B] = f(value)
+    override def apply[B](cfab: BoolResult[(A) => B]): BoolResult[B] = (this, cfab) match {
+      case (_, TrueResult(f)) => TrueResult(f(value))
+      case (_, FalseResult()) => FalseResult()
+    }
+    override def map2[B, C](cb: BoolResult[B])(f: (A, B) => C): BoolResult[C] = (this, cb) match {
+      case (_, TrueResult(b)) => TrueResult(f(value, b))
+      case (_, FalseResult()) => FalseResult()
+    }
   }
   case class FalseResult[A]() extends BoolResult[A] {
     override def map[B](f: (A) => B): BoolResult[B] = FalseResult()
     override def flatMap[B](f: (A) => BoolResult[B]): BoolResult[B] = FalseResult()
+    override def apply[B](cfab: BoolResult[(A) => B]): BoolResult[B] = FalseResult()
+    override def map2[B, C](cb: BoolResult[B])(f: (A, B) => C): BoolResult[C] = FalseResult()
   }
 
   def applyDiscount(cartId: CartId, storage: Storage[Cart]): Unit = {
