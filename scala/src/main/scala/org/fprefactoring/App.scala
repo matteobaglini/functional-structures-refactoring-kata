ae068f68ff97dfb361ea50675d8b3297c3e03f61
Matteo Baglini <matteo.baglini@gmail.com>
2017-11-27 19:13:22 +0100

    Refactor outer 'if', use 'loadResult' instead of 'cart'

--- scala/src/main/scala/org/fprefactoring/App.scala
+++ scala/src/main/scala/org/fprefactoring/App.scala
@@ -6,21 +6,24 @@ object App {
 
   trait BoolResult[+A] {
     def modify[B](f: A => B): BoolResult[B]
+    def modifyAndReduce[B](f: A => BoolResult[B]): BoolResult[B]
   }
   case class TrueResult[A](value: A) extends BoolResult[A] {
     override def modify[B](f: (A) => B): BoolResult[B] = TrueResult(f(value))
+    override def modifyAndReduce[B](f: (A) => BoolResult[B]): BoolResult[B] = f(value)
   }
   case class FalseResult[A]() extends BoolResult[A] {
     override def modify[B](f: (A) => B): BoolResult[B] = FalseResult()
+    override def modifyAndReduce[B](f: (A) => BoolResult[B]): BoolResult[B] = FalseResult()
   }
 
   def applyDiscount(cartId: CartId, storage: Storage[Cart]): Unit = {
     val cart = loadCart(cartId)
     val loadResult = if (cart != Cart.missingCart) TrueResult(cart) else FalseResult()
     if (cart != Cart.missingCart) {
-      val rule = lookupDiscountRuleResult(cart)
-      val discount = rule.modify(r => r(cart))
-      val updatedCart = discount.modify(d => updateAmount(cart, d))
+      val rule = loadResult.modifyAndReduce(c => lookupDiscountRuleResult(c))
+      val discount = loadResult.modifyAndReduce(c => rule.modify(r => r(c)))
+      val updatedCart = loadResult.modifyAndReduce(c => discount.modify(d => updateAmount(c, d)))
       updatedCart.modify(uc => save(uc, storage))
     }
   }
